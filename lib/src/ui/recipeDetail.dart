5b0e308437c837062cf23dc3f4fea28c98661cc1
Miguel <mcaraballodelarosa@gmail.com>
2020-08-01 02:15:30 +0200

    Added icon to recipe detail #1

--- lib/src/ui/recipeDetail.dart
+++ lib/src/ui/recipeDetail.dart
@@ -2,7 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youm/src/models/recipeModel.dart';
 
-class RecipeDetail extends StatelessWidget {
+class RecipeDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RecipeDetailState();
+}
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  IconData _favoriteIcon = Icons.favorite_border;
+
   @override
   Widget build(BuildContext context) {
     final RecipeModel recipe = ModalRoute.of(context).settings.arguments;
@@ -10,6 +17,15 @@ class RecipeDetail extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(recipe.title),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(_favoriteIcon, color: Colors.red),
+              onPressed: () {
+                setState(() {
+                  _favoriteIcon = Icons.favorite;
+                });
+              })
+        ],
       ),
       body: Container(
         alignment: Alignment.topLeft,
